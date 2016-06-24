0f23e5690635f4feaba0bf6bd4c75afc68c04f26
zfz0126 <zhangfengze1@gmail.com>
2016-06-23 22:58:55 -0400

    Update a1.sql

--- a1.sql
+++ a1.sql
@@ -1,36 +1,48 @@
+CREATE TYPE time_of_day AS ENUM ('morning', 'day', 'evening');
+
 CREATE DOMAIN ranking AS int
     CONSTRAINT valid_ranking CHECK(
         VALUE BETWEEN 1 AND 5);
 
-    CREATE TABLE Student (
-	user_name varchar(20) PRIMARY KEY,
-	date_of_birth date,
-	gender varchar(1),
-	country_of_birth varchar(20),
-	year_and_month_in_university date
+CREATE TABLE Student (
+	user_name  varchar(20) NOT NULL PRIMARY KEY,
+	date_of_birth date NOT NULL
+		CHECK (date_of_birth > 19000101 AND date_of_birth < 20160623),
+	gender varchar(1) NOT NULL
+		CHECK (gender = 'F' OR gender = 'M'),
+	country_of_birth varchar(20) NOT NULL,
+	date_enrolled date NOT NULL
+		CHECK (date_enrolled >19000101 AND date_enrolled<20160623)
 );
 
 CREATE TABLE Job (
-	student_name varchar(20),
-	title varchar(20),
-	company_name varchar(20),
-	start_date date,
-	end_date date,
+	student_name varchar(20) NOT NULL,
+	title varchar(20) NOT NULL,
+	company_name varchar(20) NOT NULL,
+	start_date date NOT NULL
+		CHECK (start_date > 19000101 AND start_date < 20160623),
+	end_date date NOT NULL
+		CHECK (end_date > 19000101 AND end_date < 20200101),
 	PRIMARY KEY (title, company_name),
 	FOREIGN KEY(student_name) REFERENCES Student(user_name)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
-CREATE TABLE Company_expertise (
-	company_name varchar(20),
-	expertise_name varchar(20),
+CREATE TABLE company_expertise (
+	company_name varchar(20) NOT NULL,
+	expertise_name varchar(20) NOT NULL,
 	PRIMARY KEY(company_name, expertise_name),
 	FOREIGN KEY(company_name) REFERENCES Job(company_name)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 );
 
 CREATE TABLE Student_skill (
 	title varchar(20),
 	student_name varchar(20),
 	FOREIGN KEY(student_name) REFERENCES Student(user_name)
+	    ON DELETE CASCADE
 	    ON UPDATE CASCADE,
 	level_of_expertise int
 	    CONSTRAINT check_level CHECK(level_of_expertise BETWEEN 2 AND 5),
@@ -38,36 +50,48 @@ CREATE TABLE Student_skill (
 );
 
 CREATE TABLE Edition (
+	edition_id int PRIMARY KEY,
 	course_code int
 	    CONSTRAINT 3_or_4_digit_course CHECK (course_code BETWEEN 100 AND 9999),
 	dept_code varchar(3)
 	    CONSTRAINT check_uppercase CHECK(dept_code = UPPER(dept_code)),
-	instructor_name varchar(20),
-	start_date date /*add constraint*/,
-	end_date date /*add constraint*/,
-	time_of_day varchar(10) NOT NULL /*add constraint*/,
+	start_date date NOT NULL
+		CHECK (start_date > 19000101 AND start_date < 20160623),
+	end_date date NULL
+		CHECK (end_date > 19000101 AND end_date < 20170701),
+	course_time time_of_day NOT NULL,
 	number_of_students int NOT NULL
 	    CONSTRAINT positive_enrollment CHECK (number_of_students >= 0),
-	FOREIGN KEY(instructor_name) REFERENCES Instructor(name)
-	    ON UPDATE CASCADE,
 	FOREIGN KEY(course_code, dept_code) REFERENCES Courses(course_code, dept_code)
-	    ON UPDATE CASCADE,
-	PRIMARY KEY(course_code, dept_code, instructor_name, start_date, end_date)
+	    ON UPDATE CASCADE
 );
 
 CREATE TABLE Instructor (
-	name varchar(20),
+	name varchar(30),
 	age int
 	    CONSTRAINT positive_age CHECK (age > 0),
 	gender varchar(1) NOT NULL
-	    CONSTRAINT valid_gender CHECK (gender = 'F' or 'M'),
+	    CONSTRAINT valid_gender CHECK (gender = 'F' OR gender = 'M'),
 	areas_of_expertise varchar(30),
-	faculty_or_freelancer /*add constraint*/,
-	yr_employed int NOT NULL
+	faculty_or_freelancer varchar(2)
+		CHECK (faculty_or_freelancer = 'F' OR faculty_or_freelancer = 'FR'),
+	yr_employed int
 	    CONSTRAINT valid_yr_employed CHECK (yr_employed >= 1900),
-	teaching_or_research  /*add constraint*/,
-	research_interests varchar(20) NOT NULL,
+	teaching_or_research  varchar(1)
+		CHECK (teaching_or_research = 'T' OR teaching_or_research = 'R'),
+	research_interests varchar(20),
+	CHECK ((faculty_or_freelancer = 'F' AND year_employed IS NOT NULL AND teaching_or_research = 'T' AND research_interests IS NULL) OR (faculty_or_freelancer = 'F' AND year_employed IS NOT NULL AND teaching_or_research = 'R' AND research_interests IS NOT NULL) OR (faculty_or_freelancer = 'FR' AND year_employed IS NULL AND teaching_or_research IS NULL AND research_interests IS NULL)),
+	PRIMARY KEY(name, age),
 	PRIMARY KEY(name, age)
+);
+
+CREATE TABLE Edition_to_Instructor (
+	edition_id int,
+	instructor_name varchar(20) NOT NULL,
+	age int,
+	FOREIGN KEY(edition_id) REFERENCE Edition(edition_id),
+	FOREIGN KEY(instructor_name, age) REFERENCE Instructor (name, age),
+	PRIMARY KEY(edition_id, instructor_name, age)
 );
 
 CREATE TABLE Experience (
