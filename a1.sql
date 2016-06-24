22d4c2b9200e9e07e28f47f90c07640305e6ab54
thiadeliria <cynthiapod@inside-212-89.wireless.utoronto.ca>
2016-06-23 23:20:16 -0400

    minor edits
    removed NOT NULLs, made small formatting changes

--- a1.sql
+++ a1.sql
@@ -5,23 +5,23 @@ CREATE DOMAIN ranking AS int
         VALUE BETWEEN 1 AND 5);
 
 CREATE TABLE Student (
-	user_name  varchar(20) NOT NULL PRIMARY KEY,
-	date_of_birth date NOT NULL
+	user_name  varchar(20) PRIMARY KEY,
+	date_of_birth date
 		CHECK (date_of_birth > 19000101 AND date_of_birth < 20160623),
-	gender varchar(1) NOT NULL
+	gender varchar(1)
 		CHECK (gender = 'F' OR gender = 'M'),
 	country_of_birth varchar(20) NOT NULL,
-	date_enrolled date NOT NULL
+	date_enrolled date
 		CHECK (date_enrolled >19000101 AND date_enrolled<20160623)
 );
 
 CREATE TABLE Job (
-	student_name varchar(20) NOT NULL,
-	title varchar(20) NOT NULL,
-	company_name varchar(20) NOT NULL,
-	start_date date NOT NULL
+	student_name varchar(20),
+	title varchar(20),
+	company_name varchar(20),
+	start_date date
 		CHECK (start_date > 19000101 AND start_date < 20160623),
-	end_date date NOT NULL
+	end_date date
 		CHECK (end_date > 19000101 AND end_date < 20200101),
 	PRIMARY KEY (title, company_name),
 	FOREIGN KEY(student_name) REFERENCES Student(user_name)
@@ -29,9 +29,9 @@ CREATE TABLE Job (
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE company_expertise (
-	company_name varchar(20) NOT NULL,
-	expertise_name varchar(20) NOT NULL,
+CREATE TABLE Company_expertise (
+	company_name varchar(20),
+	expertise_name varchar(20),
 	PRIMARY KEY(company_name, expertise_name),
 	FOREIGN KEY(company_name) REFERENCES Job(company_name)
 		ON DELETE CASCADE
@@ -55,12 +55,12 @@ CREATE TABLE Edition (
 	    CONSTRAINT 3_or_4_digit_course CHECK (course_code BETWEEN 100 AND 9999),
 	dept_code varchar(3)
 	    CONSTRAINT check_uppercase CHECK(dept_code = UPPER(dept_code)),
-	start_date date NOT NULL
+	start_date date
 		CHECK (start_date > 19000101 AND start_date < 20160623),
-	end_date date NULL
+	end_date date
 		CHECK (end_date > 19000101 AND end_date < 20170701),
 	course_time time_of_day NOT NULL,
-	number_of_students int NOT NULL
+	number_of_students int
 	    CONSTRAINT positive_enrollment CHECK (number_of_students >= 0),
 	FOREIGN KEY(course_code, dept_code) REFERENCES Courses(course_code, dept_code)
 	    ON UPDATE CASCADE
@@ -70,24 +70,25 @@ CREATE TABLE Instructor (
 	name varchar(30),
 	age int
 	    CONSTRAINT positive_age CHECK (age > 0),
-	gender varchar(1) NOT NULL
+	gender varchar(1)
 	    CONSTRAINT valid_gender CHECK (gender = 'F' OR gender = 'M'),
 	areas_of_expertise varchar(30),
 	faculty_or_freelancer varchar(2)
-		CHECK (faculty_or_freelancer = 'F' OR faculty_or_freelancer = 'FR'),
+		CHECK (faculty_or_freelancer = 'FA' OR faculty_or_freelancer = 'FR'),
 	yr_employed int
 	    CONSTRAINT valid_yr_employed CHECK (yr_employed >= 1900),
 	teaching_or_research  varchar(1)
 		CHECK (teaching_or_research = 'T' OR teaching_or_research = 'R'),
-	research_interests varchar(20),
-	CHECK ((faculty_or_freelancer = 'F' AND year_employed IS NOT NULL AND teaching_or_research = 'T' AND research_interests IS NULL) OR (faculty_or_freelancer = 'F' AND year_employed IS NOT NULL AND teaching_or_research = 'R' AND research_interests IS NOT NULL) OR (faculty_or_freelancer = 'FR' AND year_employed IS NULL AND teaching_or_research IS NULL AND research_interests IS NULL)),
-	PRIMARY KEY(name, age),
+	research_interests varchar(20)
+	    CHECK ((faculty_or_freelancer = 'FA' AND yr_employed IS NOT NULL AND teaching_or_research = 'T' AND research_interests IS NULL) OR 
+	    	(faculty_or_freelancer = 'FA' AND year_employed IS NOT NULL AND teaching_or_research = 'R' AND research_interests IS NOT NULL) OR 
+	    	(faculty_or_freelancer = 'FR' AND year_employed IS NULL AND teaching_or_research IS NULL AND research_interests IS NULL)),
 	PRIMARY KEY(name, age)
 );
 
 CREATE TABLE Edition_to_Instructor (
 	edition_id int,
-	instructor_name varchar(20) NOT NULL,
+	instructor_name varchar(20),
 	age int,
 	FOREIGN KEY(edition_id) REFERENCE Edition(edition_id),
 	FOREIGN KEY(instructor_name, age) REFERENCE Instructor (name, age),
