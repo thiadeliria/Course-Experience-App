dbf55f9a39c2a5b838b0bcf80a91b4473762c44d
thiadeliria <thiadeliria@users.noreply.github.com>
2016-06-26 02:15:11 -0400

    completed Edition_to_Instructor

--- a1_insert_c.sql
+++ a1_insert_c.sql
@@ -6,14 +6,15 @@ INSERT INTO Edition (edition_id, start_date, end_date, course_time, number_of_st
   (9, 20160115, 20161227, 'day', 360, 258, 'CSC'),
   (10, 20160114, 20160429, 'evening', 220, 337, 'PHL')
   
-/*do i need to repeat*/
 INSERT INTO Instructor (name, gender, age, areas_of_expertise, faculty_or_freelancer, year_employed, teaching_or_research, research_interests) VALUES
   ('Michael H. Khan', 'M', 40, 'financial accounting, auditing', 'FA', 2009, 'T', NULL),
   ('Sean Uppal', 'M', 45, 'linear algebra', 'FA', 2003, 'T', NULL),
   ('Vincent Shen', 'M', 61, 'Chinese philosophy', 'FA', 2000, 'T', NULL),
   ('Xander Chin', 'M', 30, 'computer hardware', 'FR', 2012, 'T', NULL),
-  ('Vincent Shen', 'M', 61, 'Chinese philosophy', 'FA', 2000, 'T', NULL)
   
 INSERT INTO Edition_to_Instructor(edition_id, instructor_name, age) VALUES
-  (6, 'Konstantinos N. Plataniotis', 45),
-  (7, 'Hamid S. Timorabadi', 40),
+  (6, 'Michael H. Khan', 40),
+  (7, 'Sean Uppal', 45),
+  (8, 'Vincent Shen', 61),
+  (9, 'Xander Chin', 30),
+  (10, 'Vincent Shen', 61)
